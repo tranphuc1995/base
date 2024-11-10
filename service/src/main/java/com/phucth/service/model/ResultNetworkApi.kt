ac8982445d075157a2f96a18c74901b240ffe253
tranphuc1995 <tranphuc23011995@gmail.com>
2024-11-10 20:20:19 +0700

    remove extension ResultNetworkApi

--- service/src/main/java/com/phucth/service/model/ResultNetworkApi.kt
+++ service/src/main/java/com/phucth/service/model/ResultNetworkApi.kt
@@ -4,7 +4,3 @@ data class ResultNetworkApi<T>(
     var modelResponseFromApi: T? = null,
     val error: ErrorMessageApi? = null
 )
-
-fun <T, X> ResultNetworkApi<T>.mapToModelUi(modelApi: T, map: (T) -> X): X {
-    return map(modelApi)
-}

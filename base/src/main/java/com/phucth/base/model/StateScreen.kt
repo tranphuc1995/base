7999df5da08c7a3389ae7ae4dd8d8789c1d101a0
tranphuc1995 <tranphuc23011995@gmail.com>
2024-11-24 16:12:40 +0700

    update state screen seal class

--- base/src/main/java/com/phucth/base/model/StateScreen.kt
+++ base/src/main/java/com/phucth/base/model/StateScreen.kt
@@ -1,6 +1,6 @@
 package com.phucth.base.model
 
-sealed class StateScreen<T> {
+sealed class StateScreen<out T> {
     data object Loading : StateScreen<Nothing>()
     data class DataScreen<T>(val data: T) : StateScreen<T>()
     data class Error(val messageApi: ErrorMessageApi) : StateScreen<Nothing>()
